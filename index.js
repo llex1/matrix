708a6f2f303e1ed1830ad85a98621f3594c4449b
Alexander Tsurka <68517505+llex1@users.noreply.github.com>
2021-09-13 18:30:22 +0300

    done

--- index.js
+++ index.js
@@ -3,25 +3,42 @@ const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-  // terminal: false
+  terminal: false
 });
-const inputs = [];
+const answers = [];
 const questions = [
   'Please, input a string of the letters: ',
   'And now, write a word for determining: '
 ]
 
-rl.question(questions[inputs.length], answer => {
-  inputs.push(answer)
-  rl.question(questions[inputs.length], answer => {
-    inputs.push(answer)
+rl.question(questions[answers.length], input => {
+  answers.push(input.toUpperCase())
+  rl.question(questions[answers.length], input => {
+    answers.push([...input.toUpperCase()])
     rl.close()
     result()
   })
 })
 
-const result = function() {
-  console.log(inputs);
+function result() {
+  //shape of matrix: (4*4 or 3*3)
+  const shape = Math.ceil(Math.sqrt(answers[0].length))
+  const matrix = []
+  const target = []
+  
+  for(let i=0; i<shape; i++){
+    matrix.push(answers[0].slice(i*shape, shape+i*shape))
+  }
+  answers[1].forEach((el, idx)=>{
+    for(let i=0; i<matrix.length; i++){
+      if(matrix[i].includes(el)){
+        target[idx] = [i, matrix[i].indexOf(el)]
+      }
+    }
+  })
+  console.log('===============================');
+  matrix.forEach((el)=> console.log(el))
+  console.log(target);
 }
 
 
